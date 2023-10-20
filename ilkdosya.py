b53639652f917d950568fe6002dccc30caf642a4
ZhrDemir <zdemir2702@gmail.com>
2023-10-20 22:50:10 +0300

    ilkkodlarim

---
+++ ilkdosya.py
@@ -0,0 +1,16 @@
+sayi = 20
+sayi1= 23
+isim = 'zehra'
+soyisim = 'demir'
+print (sayi)
+print("demir")
+print(isim)
+print(sayi+sayi1)
+print(sayi1*sayi-2)
+print("\n"+" merhaba phython")
+print('my name is {}'.format(isim))
+print('my name is {} {}'.format(isim,soyisim))
+print('my name is {1} {0}'.format(isim,soyisim))
+print('my name is {s} {n}'.format(s=isim,n=soyisim))
+result = 400/200
+print('result {}'.format(result))
